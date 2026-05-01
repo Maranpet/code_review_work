2f8c37da9b83ee723a127138efd7ff9ec99a9bbc
Maranpet <masha-1606@yandex.ru>
2026-05-01 12:29:27 +0300

    feat: реализация alculator и тестов

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,33 @@
 public class Calculator {
+
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
+
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
+
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return a / b;
     }
+
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
+
+    // Формула: (a + b) * c - d / e
     public int solver(){
-        //TODO inser your realisation in method solver
+        int a = 10, b = 5, c = 2, d = 8, e = 4;
+
+        int sum = add(a, b);              // 10 + 5 = 15
+        int product = times(sum, c);      // 15 * 2 = 30
+        int quotient = div(d, e);         // 8 / 4 = 2
+        int result = dif(product, quotient); // 30 - 2 = 28
+
+        return result;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,31 +1,44 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    private final Calculator calc = new Calculator();
+
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(5, calc.add(2, 3), "2 + 3 должно равняться 5");
+        assertEquals(0, calc.add(-1, 1), "-1 + 1 должно равняться 0");
+        assertEquals(-5, calc.add(-2, -3), "-2 + -3 должно равняться -5");
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(1, calc.dif(4, 3), "4 - 3 должно равняться 1");
+        assertEquals(-5, calc.dif(2, 7), "2 - 7 должно равняться -5");
+        assertEquals(0, calc.dif(5, 5), "5 - 5 должно равняться 0");
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(3, calc.div(9, 3), "9 / 3 должно равняться 3");
+        assertEquals(0, calc.div(1, 2), "1 / 2 (целочисленное) должно равняться 0");
+
+        // Проверка исключения при делении на ноль
+        assertThrows(ArithmeticException.class, () -> calc.div(5, 0),
+                "Деление на ноль должно вызывать ArithmeticException");
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(6, calc.times(2, 3), "2 * 3 должно равняться 6");
+        assertEquals(0, calc.times(0, 100), "0 * 100 должно равняться 0");
+        assertEquals(-12, calc.times(3, -4), "3 * -4 должно равняться -12");
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        // Проверяем результат формулы: (10 + 5) * 2 - 8 / 4 = 28
+        assertEquals(28, calc.solver(), "Результат solver() должен быть 28");
     }
 }
