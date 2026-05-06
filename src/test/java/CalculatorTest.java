324fd880d8640cf073f6da659cb482c3b0e34606
Шульга Виктория Васильенва <karewity@mail.ru>
2026-05-06 18:53:13 +0300

    fix: удален закомментированный код

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -24,7 +24,6 @@ class CalculatorTest {
         assertEquals(3, calc.div(9, 3), "9 / 3 должно равняться 3");
         assertEquals(0, calc.div(1, 2), "1 / 2 (целочисленное) должно равняться 0");
 
-        // Проверка исключения при делении на ноль
         assertThrows(ArithmeticException.class, () -> calc.div(5, 0),
                 "Деление на ноль должно вызывать ArithmeticException");
     }
@@ -38,41 +37,6 @@ class CalculatorTest {
 
     @Test
     void solver() {
-        // Проверяем результат формулы: (10 + 5) * 2 - 8 / 4 = 28
         assertEquals(28, calc.solver(), "Результат solver() должен быть 28");
     }
 }
-
-
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class CalculatorTest {
-//
-//    Calculator calc = new Calculator();
-//
-//    @Test
-//    void add() {
-//        assertEquals(5, calc.add(2, 3));
-//    }
-//
-//    @Test
-//    void dif() {
-//        assertEquals(1, calc.dif(3, 2));
-//    }
-//
-//    @Test
-//    void div() {
-//        assertEquals(2, calc.div(4, 2));
-//    }
-//
-//    @Test
-//    void times() {
-//        assertEquals(6, calc.times(2, 3));
-//    }
-//
-//    @Test
-//    void solver() {
-//        assertEquals(25, calc.solver());
-//    }
-//}
