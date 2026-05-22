caee1591889a4cef97ce8515e6a68790a111c8a3
karewity <karewity@mail.ru>
2026-05-22 13:57:31 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -19,7 +19,6 @@ public class Calculator {
         return a * b;
     }
 
-    // Формула: (a + b) * c - d / e
     public int solver(){
         int a = 10, b = 5, c = 2, d = 8, e = 4;
 
@@ -27,15 +26,6 @@ public class Calculator {
         int product = times(sum, c);
         int quotient = div(d, e);
         int result = dif(product, quotient);
-
-        return result;
-    }
-}
-
-        int sum = add(a, b);              // 10 + 5 = 15
-        int product = times(sum, c);      // 15 * 2 = 30
-        int quotient = div(d, e);         // 8 / 4 = 2
-        int result = dif(product, quotient); // 30 - 2 = 28
 
         return result;
     }
